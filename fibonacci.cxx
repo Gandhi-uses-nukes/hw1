240719524f92f2d4ccdca6ad11c3ea406a3cad34
Gandhi-uses-nukes <timvogel@posteo.de>
2015-10-28 18:56:45 +0000

    Entschuldigung, aber mein Tausendfüßler muss hier mal durch ....
    ......╚⊙ ⊙╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝
    ..╚═(███)═╝
    .╚═(███)═╝
    ╚═(███)═╝
    ╚═(███)═╝
    .╚═(███)═╝
    ..╚═(███)═╝
    ...╚═(███)═╝
    ...╚═(███)═╝

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int N;
+
+int fibonacci(int N){
+	if(N == 0){
+		return 0;
+	}
+	if(N == 1){
+		return 1;
+	}
+	else{
+		return ( fibonacci(N-1) + fibonacci(N-2) );
+	}
+}
+
+int main(){
+	cout << "Bitte geben Sie eine Zahl ein: \n";
+	cin >> N;
+	cout << "Die Fibonacci-Zahl ist: " << fibonacci(N) << endl;
+	return 0;
+}
